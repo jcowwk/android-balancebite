d2f039f00e250644e9c00ec01b803dc3c9df92bc
jcowwk <jcowwk@gmail.com>
2024-05-10 00:10:28 +0900

    feat: 버튼 클릭 기능 테스트

--- app/src/main/java/org/techtown/balancebite/SettingFragment.java
+++ app/src/main/java/org/techtown/balancebite/SettingFragment.java
@@ -7,12 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SettingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_setting, container, false);
+
+        Button button = rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "메세지", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
     }
